b68427d068c89d980dd2de3614b1574bcf5b568c
ferhm <fernando.gr0197@gmail.com>
2017-04-04 16:45:34 +0200

    fernando

--- Practica2/unimodal.cpp
+++ Practica2/unimodal.cpp
@@ -7,24 +7,36 @@ int unimodal(vector<int> v){
   bool fin=false;
   int tamanio=v.size()-1;
   int indice=tamanio/2;
+	int aux;
+	int contador=0;
   while(!fin){
     if(v.at(indice-1)<v.at(indice))
        if(v.at(indice+1)<v.at(indice))
           fin=true;
        else
           indice+=(tamanio-indice)/2;
-    else
-       indice=(tamanio-indice)/2;
+    else{
+			aux=tamanio;
+			tamanio=indice;
+    	indice=(aux-indice)/2;
+	 	}
+		contador++;
   }
+	cout << "El contador es: " << contador << endl;
   return indice;
  }
 
 int main(){
   vector<int> array;
 
-  array.push_back(1);
-  array.push_back(10);
-  array.push_back(3);
+int tamanio=16;
+int posicion=9;
+for (int i=0, j=0; i<tamanio;i++,j--){
+	if(i<=posicion)
+	   array.push_back(i);
+	else
+	   array.push_back(j);
+}
 
 
   cout << "El vector contiene: " << endl;
